95423612dc1b1e9bc95bd70b015a28be570ddd7b
Milindi-Kodikara <milindikodikara@gmail.com>
2021-09-04 12:29:51 +1000

    Add custom welcome message to newcomers

--- index.js
+++ index.js
@@ -4,31 +4,45 @@ const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const { MessageEmbed } = require('discord.js');
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.once('ready', () => {
-	console.log('Ready!');
+// EVENTS Available: https://gist.github.com/koad/316b265a91d933fd1b62dddfcc3ff584
+
+// When a new user enters
+client.on('guildMemberAdd', member => {
+
+    const channel = member.guild.channels.cache.find(channel => channel.name === "introductions")
+    if (!channel) return;
+
+    const newMemberEmbed = new MessageEmbed()
+    .setTitle(`Well hello there ${member}!`)
+    .setDescription(`\nWelcome to our funky community \u2728 \u2728 \u2728 \nDo tell us a little bit about yourself 
+    and then make sure to hop into <#861451844856184867> and answer the Question of the day!\n
+    Oh, and don't forget to checkout our upcoming events here in <#861456370012127242> \n\nHave fun :watermelon:`)
+    .setColor("#a806a4")
+
+    channel.send({ embeds: [newMemberEmbed] })
 });
 
+// Code for the slash commands
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
 	const { commandName } = interaction;
 
 	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
+		await interaction.reply('pong \u2728 :watermelon:');
 	} else if (commandName === 'server') {
 		await interaction.reply(`Well well well . . . you wanna know about this server huh! \n\nServer name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
 	} else if (commandName === 'user') {
 		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
 	}
 });
-
-client.login(token);
 
 // Login to Discord with your client's token
 client.login(token);
